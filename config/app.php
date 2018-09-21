8cf1c9a8e52c88b9c29ccd51d0cd251f0bee6d8e
Unknown <mau.kort.alt@hotmail.com>
2018-09-21 22:42:12 +0200

    config update

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/crimefreecities/public'),
+    'url' => env('APP_URL', 'https:://crimefreecities.com'),
 
     /*
     |--------------------------------------------------------------------------
